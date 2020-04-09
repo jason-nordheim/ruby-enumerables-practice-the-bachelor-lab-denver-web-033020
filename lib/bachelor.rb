4e5f340e7b7a9da22f2a1d6cfe78d79f8aa6ffda
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-09 20:31:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ require 'pp'
 
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season].each { |k,v| p v }
+  data[season].each { |hash| p hash }
 end
 
 def get_contestant_name(data, occupation)
