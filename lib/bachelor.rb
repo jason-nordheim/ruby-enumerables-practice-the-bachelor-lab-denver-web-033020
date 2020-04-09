6ff1744074b0234a57e178684dfd6bbadf7396e7
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-09 20:35:31 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,8 +6,9 @@ def get_first_name_of_season_winner(data, season)
   saved = nil
   data[season].each do |hash|
     if hash[:status] == "Winner"
+      p hash 
     end
-  end 
+  end
 end
 
 def get_contestant_name(data, occupation)
