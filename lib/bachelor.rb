3a32165b65aca20d00f8429f7ad90f37f715fc88
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-09 20:34:01 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ require 'pp'
 
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season].select { |hash| hash[:status] == "Winner"}[0][:name]
+  data[season].select { |hash| hash[:status] == "Winner"}[:name]
 end
 
 def get_contestant_name(data, occupation)
